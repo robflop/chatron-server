a8ad5e08036111ea473b681c14a102ea89ea1f0f
robflop <robflop98@gmail.com>
2017-06-15 05:23:51 +0200

    server-side user object updates

--- src/server.js
+++ src/server.js
@@ -1,15 +1,7 @@
-const config = require('./config.json');
 const moment = require('moment');
 const express = require('express');
 const { writeFileSync } = require('fs');
-
-const knex = require('knex')({
-	client: 'sqlite3',
-	useNullAsDefault: true,
-	connection: {
-		filename: config.databasePath
-	}
-});
+const config = require('./config.json');
 
 const server = express();
 const http = require('http').Server(server);
@@ -21,55 +13,46 @@ http.listen(config.port, () => {
 	console.log(`[${timestamp}] chatron server running on port ${config.port}!${config.SSLproxy ? ' (Proxied to SSL)' : ''}`);
 }); // info for self: listening using http because socket.io doesn't take an express instance (see socket.io docs)
 
-if (config.firstRun) {
-	// prepare the database on first run
-	knex.schema.createTableIfNotExists('channels', table => {
-		table.increments();
-		table.string('name').unique();
-		table.string('topic');
-	}).then(() => {
-		knex('channels').insert({ name: 'general', topic: 'The beginning of it all.' })
-		.then(() => null);
-	});
-
-	config.firstRun = false;
-	writeFileSync(`./config.json`, JSON.stringify(config, null, '\t'));
-}
-
-const channels = new Map();
-const usernames = [];
-
-knex.select('*').from('channels').then(rows => {
-	rows.map(channel => channels.set(channel.id, channel));
-	// console.log(channels.get(1));
-});
+const channels = {};
+const users = [];
 
 server.get('/', (req, res) => res.send('Ay, chatron server.'));
 
 io.on('connection', socket => {
 	socket.on('login', user => {
-		if (usernames.includes(user.username)) {
+		if (users.includes(user.username)) {
 			return socket.emit('duplicateUsernameError', { message: 'Username is taken.' });
 		}
 		if (user.username.length > 32 || user.username.length < 2) {
 			return socket.emit('usernameLengthError', { message: 'Username must be between 2 and 32 characters long.' });
 		}
-		user.channels.map(channel => {
-			if (channel.length > 32 || channel.length < 2) {
-				return socket.emit('channelLengthError', { message: `Channel names must be between 2 and 32 characters long. ('${channel}')` });
+
+		Object.values(user.channels).forEach(channel => {
+			if (channel.name.length > 32 || channel.name.length < 2) {
+				return socket.emit('channelNameLengthError', {
+					message: 'Channel names must be between 2 and 32 characters long.',
+					channel: channel.name
+				});
 			}
+
+			channels.hasOwnProperty(channel.name)
+			? channels[channel.name].users.push(user.username)
+			: channels[channel.name] = { name: channel.name, users: [user.username] };
+
 			return null;
 		});
-		user.channels.map(channel => { // eslint-disable-line arrow-body-style
-			return socket.to(channel).emit('systemMessage', {
+		Object.values(user.channels).forEach(channel => { // eslint-disable-line arrow-body-style
+			return socket.to(channel.name).emit('systemMessage', {
 				content: `User ${user.username} has joined.`,
 				timestamp: moment().format('YYYY-MM-DD')
 			});
 		});
-		usernames.push(user.username);
-		socket.join(user.channels);
-		console.log(`User ${user.username} connected and joined the '${user.channels.join('\', \'')}' channel(s).`);
-		socket.emit('loginSuccess', null);
+		const channelNames = Object.keys(user.channels);
+		users.push(user.username);
+		socket.join(channelNames);
+		console.log(`User ${user.username} connected and joined the '${channelNames.join('\', \'')}' channel(s).`);
+		socket.emit('channelData', channels);
+		return socket.emit('loginSuccess', null);
 	});
 
 	socket.on('logout', user => {
@@ -77,7 +60,7 @@ io.on('connection', socket => {
 		user.channels.map(channel => { // eslint-disable-line arrow-body-style
 			return socket.to(channel).emit('systemMessage', { content: `User ${user.username} has left.`, timestamp: moment().format('YYYY-MM-DD') });
 		});
-		usernames.splice(usernames.indexOf(user.username), 1);
+		users.splice(users.indexOf(user.username), 1);
 		socket.emit('logoutSuccess', null);
 	});
 
